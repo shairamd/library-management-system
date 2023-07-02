36a32d8ff76a89bb666fb6e79c0abf4068682d81
shairamd <94759233+shairamd@users.noreply.github.com>
2023-07-02 17:40:46 +0800

    Update Book.java

--- src/java/entity/Book.java
+++ src/java/entity/Book.java
@@ -2,28 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dbObject;
+package entity;
 
 /**
  *
  * @author Nguyen Minh
  */
 public class Book {
+
     private int bookid;
     private String name;
     private String author;
     private String img;
-    private String category;
+    private int category;
     private String publisher;
     private String language;
     private int total;
     private int current;
-    private String poisition;
+    private String position;
 
     public Book() {
     }
 
-    public Book(int bookid, String name, String author, String img, String category, String publisher, String language, int total, int current, String poisition) {
+    public Book(int bookid, String name, String author, String img, int category, String publisher, String language, int total, int current, String position) {
         this.bookid = bookid;
         this.name = name;
         this.author = author;
@@ -33,7 +34,20 @@ public class Book {
         this.language = language;
         this.total = total;
         this.current = current;
-        this.poisition = poisition;
+        this.position = position;
+    }
+
+    public Book(int bookid, String name, String author, int category, String publisher, String language, int total, int current, String position) {
+        this.bookid = bookid;
+        this.name = name;
+        this.author = author;
+        this.img = "img/book/book.jpg";
+        this.category = category;
+        this.publisher = publisher;
+        this.language = language;
+        this.total = total;
+        this.current = current;
+        this.position = position;
     }
 
     public int getBookid() {
@@ -68,11 +82,11 @@ public class Book {
         this.img = img;
     }
 
-    public String getCategory() {
+    public int getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(int category) {
         this.category = category;
     }
 
@@ -108,18 +122,12 @@ public class Book {
         this.current = current;
     }
 
-    public String getPoisition() {
-        return poisition;
+    public String getPosition() {
+        return position;
     }
 
-    public void setPoisition(String poisition) {
-        this.poisition = poisition;
+    public void setPosition(String position) {
+        this.position = position;
     }
 
-    @Override
-    public String toString() {
-        return "Book{" + "bookid=" + bookid + ", name=" + name + ", author=" + author + ", img=" + img + ", category=" + category + ", nxb=" + publisher + ", language=" + language + ", total=" + total + ", current=" + current + ", poisition=" + poisition + '}';
-    }
-    
-    
 }
